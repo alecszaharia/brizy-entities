14a99c9f8cfecd0f514e889ffc60968f64bc700b
Alex Zaharia <alecszaharia@gmail.com>
2021-09-13 12:13:15 +0300

    improv: Updated few libs

--- src/Entity/MetafieldVarchar.php
+++ src/Entity/MetafieldVarchar.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 namespace Brizy\Bundle\EntitiesBundle\Entity;
 
 use Brizy\Bundle\EntitiesBundle\Entity\Common\MetafieldBase;
-use Brizy\Bundle\EntitiesBundle\Repository\MetafieldBaseRepository;
+use Brizy\Bundle\EntitiesBundle\Repository\MetafieldVarcharRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\UniqueConstraint;
-use Brizy\Bundle\EntitiesBundle\Repository\MetafieldVarcharRepository;
+
 /**
  * @ORM\Table(name="metafield__varchar", uniqueConstraints={@UniqueConstraint(columns={"entity_id","metafield_id"})})
  * @ORM\Entity(repositoryClass=MetafieldVarcharRepository::class)
